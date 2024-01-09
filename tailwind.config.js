fad068b4a5a6b814a3ed75fec4b5573e62e55cdf
Brad Schiff <LearnWebCode@users.noreply.github.com>
2024-01-09 14:49:18 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./**/*.php", "./src/**/*.js"],
-  plugins: [require("@tailwindcss/typography")]
+  plugins: [require("@tailwindcss/typography")],
+  theme: {
+    extend: {
+      fontFamily: {
+        "sans": ["Inter", ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
 }
